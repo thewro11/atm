5ef2e1986569981c51153645de6e3a0dae0e80a4
Théo <thewro11@gmail.com>
2026-02-18 21:35:42 +0700

    feat: add cucumber tests

--- src/test/java/StepDefinitions.java
+++ src/test/java/StepDefinitions.java
@@ -1,10 +1,12 @@
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.tfog.atm.Atm;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class StepDefinitions {
   private Atm atm;
@@ -14,10 +16,44 @@ public class StepDefinitions {
     atm = new Atm();
   }
 
+  @When("user logs in with bank account id {string}")
+  public void user_logs_in_with_bank_account_id(String bankAccountId) throws Exception {
+    atm.login(bankAccountId);
+  }
+
+  @Then("user should deposit {double}THB into their bank account successfully")
+  public void user_should_deposit_into_their_bank_account_successfully(double money) {
+    assertDoesNotThrow(() -> {
+      atm.deposit(money);
+    });
+  }
+
+  @Then("user should not deposit {double}THB into their bank account")
+  public void user_should_not_deposit_into_their_bank_account(double money) {
+    assertThrows(Exception.class, () -> {
+      atm.deposit(money);
+    });
+  }
+
   @Then("user with bank account id {string} should be able to log in")
   public void user_with_bank_account_id_should_be_able_to_log_in(String bankAccountId) {
     assertDoesNotThrow(() -> {
       atm.login(bankAccountId);
     });
   }
+
+  @Then("user should not withdraw {double}THB from their bank account")
+  public void user_should_not_withdraw_into_their_bank_account(double money) {
+    assertThrows(Exception.class, () -> {
+      atm.withdraw(money);
+    });
+  }
+
+  @Then("user should withdraw {double}THB from their bank account successfully")
+  public void user_should_withdraw_into_their_bank_account_successfully(double money) {
+    assertDoesNotThrow(() -> {
+      atm.withdraw(money);
+    });
+  }
+
 }
--- src/test/resources/features/atm.feature
+++ src/test/resources/features/atm.feature
@@ -4,3 +4,46 @@ Feature: ATM
   Scenario: User logs in with a valid bank account
     Given start the ATM machine
     Then user with bank account id "20250928001" should be able to log in
+
+  Scenario Outline: Successful Deposits
+    Given start the ATM machine
+    When user logs in with bank account id "<bankAccountId>"
+    Then user should deposit <money>THB into their bank account successfully
+
+    Examples:
+        | bankAccountId | money |
+        | 20250928014 | 2000 |
+        | 20250928014 | 300 |
+        | 20250928014 | 25.50 |
+
+  Scenario Outline: Fail deposits
+    Given start the ATM machine
+    When user logs in with bank account id "<bankAccountId>"
+    Then user should not deposit <money>THB into their bank account
+
+    Examples:
+      | bankAccountId | money |
+      | 20250928014 | 0 |
+      | 20250928014 | -2000 |
+
+  Scenario Outline: Successful Withdraws
+    Given start the ATM machine
+    When user logs in with bank account id "<bankAccountId>"
+    Then user should withdraw <money>THB from their bank account successfully
+
+    Examples:
+      | bankAccountId | money |
+      | 20250928014 | 100 |
+      | 20250928014 | 150 |
+      | 20250928014 | 500.50 |
+
+  Scenario Outline: Fail Withdraws
+    Given start the ATM machine
+    When user logs in with bank account id "<bankAccountId>"
+    Then user should not withdraw <money>THB from their bank account
+
+    Examples:
+      | bankAccountId | money |
+      | 20250928014 | 0 |
+      | 20250928014 | -20.20 |
+      | 20250928014 | 20000000 |
