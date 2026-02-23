ee50ba69bcd70b633e5e301043e11f66d12b802e
Théo <thewro11@gmail.com>
2026-02-23 22:16:12 +0700

    feat: add Mockito

--- src/test/java/StepDefinitions.java
+++ src/test/java/StepDefinitions.java
@@ -1,7 +1,10 @@
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 
+import org.mockito.Mockito;
 import org.tfog.atm.Atm;
 
 import io.cucumber.java.en.Given;
@@ -13,7 +16,7 @@ public class StepDefinitions {
 
   @Given("start the ATM machine")
   public void start_the_atm_machine() throws Exception {
-    atm = new Atm();
+    atm = Mockito.mock();
   }
 
   @When("user logs in with bank account id {string}")
@@ -22,35 +25,45 @@ public class StepDefinitions {
   }
 
   @Then("user should deposit {double}THB into their bank account successfully")
-  public void user_should_deposit_into_their_bank_account_successfully(double money) {
+  public void user_should_deposit_into_their_bank_account_successfully(double money) throws Exception {
+    doNothing().when(atm).deposit(money);
+
     assertDoesNotThrow(() -> {
       atm.deposit(money);
     });
   }
 
   @Then("user should not deposit {double}THB into their bank account")
-  public void user_should_not_deposit_into_their_bank_account(double money) {
+  public void user_should_not_deposit_into_their_bank_account(double money) throws Exception {
+    doThrow(Exception.class).when(atm).deposit(money);
+
     assertThrows(Exception.class, () -> {
       atm.deposit(money);
     });
   }
 
   @Then("user with bank account id {string} should be able to log in")
-  public void user_with_bank_account_id_should_be_able_to_log_in(String bankAccountId) {
+  public void user_with_bank_account_id_should_be_able_to_log_in(String bankAccountId) throws Exception {
+    doNothing().when(atm).login("20250928001");
+
     assertDoesNotThrow(() -> {
       atm.login(bankAccountId);
     });
   }
 
   @Then("user should not withdraw {double}THB from their bank account")
-  public void user_should_not_withdraw_into_their_bank_account(double money) {
+  public void user_should_not_withdraw_into_their_bank_account(double money) throws Exception {
+    doThrow(Exception.class).when(atm).withdraw(money);
+
     assertThrows(Exception.class, () -> {
       atm.withdraw(money);
     });
   }
 
   @Then("user should withdraw {double}THB from their bank account successfully")
-  public void user_should_withdraw_into_their_bank_account_successfully(double money) {
+  public void user_should_withdraw_into_their_bank_account_successfully(double money) throws Exception {
+    doNothing().when(atm).withdraw(money);
+
     assertDoesNotThrow(() -> {
       atm.withdraw(money);
     });
